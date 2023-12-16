b2232ddc7b670a359c9fd0810751417c126822b5
João Victor Borges <joao.borges@discente.ufg.br>
2023-12-16 08:53:09 -0300

    codigo-pilha.h

---
+++ codigo-pilha.h
@@ -0,0 +1,11 @@
+#define TamPilha 100
+
+typedef int elem;
+typedef struct pilha pilha_t;
+
+pilha_t *create();
+void destroy(pilha_t *p);
+int verifyFull(pilha_t *p);
+int verifyEmpty(pilha_t *p);
+int push(pilha_t *p, elem x);
+int top(pilha_t *p, elem *x);
